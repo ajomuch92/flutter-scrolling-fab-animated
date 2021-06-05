f561d7b3351d4e6efe41916c0ede4e2556ad469f
Aarón Montes <aaron@kssco.com>
2021-06-05 16:36:38 -0600

    Adding implementation for animated fab button

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,2 @@
-## [0.0.1] - TODO: Add release date.
-
-* TODO: Describe initial release.
+<!--  Date Format DD/MM/YYYY -->
+## [0.0.1] - First release (05/06/2021)
--- lib/flutter_scrolling_fab_animated.dart
+++ lib/flutter_scrolling_fab_animated.dart
@@ -1,7 +1,126 @@
 library flutter_scrolling_fab_animated;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+/// Widget to animate the button when scroll down
+class ScrollingFabAnimated extends StatefulWidget {
+  /// Function to use when on press the button
+  final Function onPress;
+
+  /// Double value to set the button elevation
+  final double elevation;
+
+  /// Double value to set the button width
+  final double width;
+
+  /// Double value to set the button height
+  final double height;
+
+  /// Value to set the duration for animation
+  final Duration duration;
+
+  /// Widget to use as button icon
+  final Widget icon;
+
+  /// Widget to use as button text when button is expanded
+  final Widget text;
+
+  /// Value to set the curve for animation
+  final Curve curve;
+
+  /// ScrollController to use 
+  final ScrollController scrollController;
+
+  /// Double value to set the boundary value when scroll animation is triggered
+  final double limitIndicator;
+
+  /// Color to set the button background color
+  final Color color;
+
+  ScrollingFabAnimated({Key key,
+    @required this.icon,
+    @required this.text,
+    @required this.onPress,
+    @required this.scrollController,
+    this.elevation = 5.0,
+    this.width = 120.0,
+    this.height = 60.0,
+    this.duration = const Duration(milliseconds: 500),
+    this.curve,
+    this.limitIndicator = 10.0,
+    this.color = Colors.blueAccent
+  }) 
+    : super(key: key);
+
+  @override
+  _ScrollingFabAnimatedState createState() => _ScrollingFabAnimatedState();
+}
+
+class _ScrollingFabAnimatedState extends State<ScrollingFabAnimated> {
+  bool _onTop = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _handleScroll();
+  }
+
+  @override
+  void dispose() {
+    widget.scrollController.removeListener(() {});
+    super.dispose();
+  }
+
+  /// Function to add listener for scroll
+  void _handleScroll() {
+    ScrollController _scrollController = widget.scrollController;
+    _scrollController.addListener(() {
+      print(_scrollController.position.userScrollDirection);
+      if(_scrollController.position.pixels > widget.limitIndicator && _scrollController.position.userScrollDirection ==
+          ScrollDirection.reverse) {
+        setState(() {
+          _onTop = false;
+        });
+      } else if (_scrollController.position.pixels <= widget.limitIndicator && _scrollController.position.userScrollDirection ==
+          ScrollDirection.forward) {
+        print(_scrollController.position.pixels);
+        setState(() {
+          _onTop = true;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: widget.onPress,
+      child: Card(
+        elevation: widget.elevation,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(widget.height / 2)),
+        ),
+        child: AnimatedContainer(
+          curve: widget.curve??Curves.easeInOut,
+          duration: widget.duration,
+          height: widget.height,
+          width: _onTop? widget.width: widget.height,
+          decoration: BoxDecoration(
+            color: widget.color,
+            borderRadius: BorderRadius.all(Radius.circular(widget.height / 2))
+          ),
+          child: _onTop? 
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                widget.icon,
+                widget.text
+              ],
+            ): 
+            widget.icon,
+        ),
+      ),
+    );
+  }
 }
