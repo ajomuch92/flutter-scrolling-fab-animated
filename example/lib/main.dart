63f5079ec0be298120866db59d3360d098a6e327
Aarón J. Montes <32050436+ajomuch92@users.noreply.github.com>
2024-03-29 13:45:32 -0600

    refactor: 💡 Adding nullish operator for key parameter

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
 }
 
 class Home extends StatefulWidget {
-  Home({Key key}) : super(key: key);
+  Home({Key? key}) : super(key: key);
 
   @override
   _HomeState createState() => _HomeState();
